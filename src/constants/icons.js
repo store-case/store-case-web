82d0cd3c146473a781860cf4f425680a6b2e3541
honee8583 <ekgns0508@gmail.com>
2025-10-23 09:03:00 +0900

    fix: 아이콘 위치 수정

--- src/constants/icons.js
+++ src/constants/icons.js
@@ -1,16 +1,16 @@
 const ICONS = {
-  arrowLeft: 'http://localhost:3845/assets/a02e1850da7e623a9d373e216280d7ebf20899ec.svg',
-  chevronRight: 'http://localhost:3845/assets/a28e2400833f24828319a4bce38e4065cef6ffea.svg',
-  user: 'http://localhost:3845/assets/ba189415d5ac91f6db2fb7b060d682df669fd8bb.svg',
-  mail: 'http://localhost:3845/assets/cc9b2186fe4b7557a4368f1cd4d4a8d52aa6ed0e.svg',
-  lock: 'http://localhost:3845/assets/1df7634ad8f2dd8dd8f9b16357f63607ac0bc1e3.svg',
-  eyeOff: 'http://localhost:3845/assets/cd4343ee4e3e04e78623d6128f0c03680dcfe153.svg',
-  key: 'http://localhost:3845/assets/dda1679edc8bd143414ad7b1d98dca524b3577d3.svg',
-  phone: 'http://localhost:3845/assets/6efc9be61ef8d7c2dd207fc0e55d53b47a10eef5.svg',
-  ruleCheck: 'http://localhost:3845/assets/b0db6b5805ef1d73283b4b62adcfb0366749099c.svg',
-  ruleError: 'http://localhost:3845/assets/cb9289dfe92aefa55aa67c7218f0c84c5b036cf3.svg',
-  checklist: 'http://localhost:3845/assets/9094a009aacabbd0774dbeba35b857cf4c1ba31e.svg',
-  google: 'http://localhost:3845/assets/a31875c212530607a21c7da31e37e11a5a770fc6.svg',
+  arrowLeft: '/public/icons/arrow-left.svg',
+  chevronRight: '/public/icons/chevron-right.svg',
+  user: '/public/icons/user.svg',
+  mail: '/public/icons/mail.svg',
+  lock: '/public/icons/lock.svg',
+  eyeOff: '/public/icons/eye-off.svg',
+  key: '/public/icons/key.svg',
+  phone: '/public/icons/phone.svg',
+  ruleCheck: '/public/icons/rule-check.svg',
+  ruleError: '/public/icons/rule-error.svg',
+  checklist: '/public/icons/check.svg',
+  google: '/public/icons/google.svg',
 }
 
 export default ICONS
